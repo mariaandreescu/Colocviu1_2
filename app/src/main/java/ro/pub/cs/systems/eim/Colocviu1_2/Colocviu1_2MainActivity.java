b0bb6d65419ee8e5642996b2a0aeddca937dae9c
mariaandreescu <maria.andreescu@stud.acs.upb.com>
2021-04-12 10:44:52 +0300

    'rezolvare A2 am mai adaugat mici modificari + B1'

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.java
@@ -3,12 +3,46 @@ package ro.pub.cs.systems.eim.Colocviu1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class Colocviu1_2MainActivity extends AppCompatActivity {
+
+    private EditText nextTermText;
+    private TextView allTermsText;
+    private Button addButton;
+
+    PressButtonListener buttonListener = new PressButtonListener();
+
+
+    private class PressButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.add_button) {
+                if (!nextTermText.getText().toString().equals("")) {
+                    if (allTermsText.getText().toString().equals("")) {
+                        allTermsText.setText(allTermsText.getText().toString() + nextTermText.getText().toString());
+                    } else {
+                        allTermsText.setText(allTermsText.getText().toString() + " + " + nextTermText.getText().toString());
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_2_main);
+
+        nextTermText = (EditText) findViewById(R.id.next_term);
+        allTermsText = (TextView) findViewById(R.id.all_terms);
+        addButton = (Button) findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(buttonListener);
     }
 }
